6f7c9dd83a1537af4f310e4cf5cd63aec0a127b5
Lonk <51008428+Lonk12@users.noreply.github.com>
2022-01-24 21:02:23 -0500

    Update MemeSounds.plugin.js

--- MemeSounds/MemeSounds.plugin.js
+++ MemeSounds/MemeSounds.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name Meme Sounds
- * @version 0.5.0
+ * @version 0.5.1
  * @description Plays Memetastic sounds depending on what is being sent in chat. This was heavily inspired by the idea of Metalloriff's bruh plugin so go check him out!
  * @invite https://discord.gg/YMqKjWEVxG
  * @author Lonk#6942
