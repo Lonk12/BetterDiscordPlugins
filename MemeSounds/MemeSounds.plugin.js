7e6edd05b6b2c7032c10d83d9754fcbc12a4760a
Lonk <51008428+Lonk12@users.noreply.github.com>
2022-01-24 20:33:49 -0500

    Update MemeSounds.plugin.js
    added the beta version of the meme sounds plugin

--- MemeSounds/MemeSounds.plugin.js
+++ MemeSounds/MemeSounds.plugin.js
@@ -1,1 +1,85 @@
+/**
+ * @name Meme Sounds
+ * @version 0.5.0
+ * @invite https://discord.gg/YMqKjWEVxG
+ * @authorLink https://github.com/Lonk12/
+ * @source https://github.com/Lonk12/BetterDiscordPlugins/blob/main/MemeSounds/MemeSounds.plugin.js
+ * @updateUrl https://raw.githubusercontent.com/Lonk12/BetterDiscordPlugins/main/MemeSounds/MemeSounds.plugin.js
+ */
 
+module.exports = (() => {
+	
+	/* Configuration */
+	const config = {info: {name: "Meme Sounds", authors: [{name: "Lonk#6942", discord_id: "557388558017495046", github_username: "Lonk12", twitter_username: "wolfyypaw"}], version: "0.5.0", description: "Plays Memetastic sounds depending on what is being sent in chat. This was heavily inspired by the idea of Metalloriff's bruh plugin so go check him out!", github: "https://github.com/Lonk12/BetterDiscordPlugins/blob/main/MemeSounds/MemeSounds.plugin.js", github_raw: "https://raw.githubusercontent.com/Lonk12/BetterDiscordPlugins/main/MemeSounds/MemeSounds.plugin.js"}, defaultConfig: [{id: "setting", name: "Sound Settings", type: "category", collapsible: true, shown: false, settings: [{id: "LimitChan", name: "Limit to the current channel only.", note: "When enabled, sound effects will only play within the currently selected channel.", type: "switch", value: true}, {id: "delay", name: "Sound effect delay.", note: "The delay in miliseconds between each sound effect.", type: "slider", value: 200, min: 10, max: 1000, renderValue: v => Math.round(v) + "ms"}]}]};
+
+	/* Library Stuff */
+	return !global.ZeresPluginLibrary ? class {
+		constructor() { this._config = config; }
+        getName() {return config.info.name;}
+        getAuthor() {return config.info.authors.map(a => a.name).join(", ");}
+        getDescription() {return config.info.description;}
+        getVersion() {return config.info.version;}
+		load() {BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {confirmText: "Download Now", cancelText: "Cancel", onConfirm: () => {require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (err, res, body) => {if (err) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js"); await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));});}});}
+		start() { }
+		stop() { }
+	} : (([Plugin, Api]) => {
+
+		const plugin = (Plugin, Api) => { try {
+			
+			/* Constants */
+			const {DiscordModules: {Dispatcher, SelectedChannelStore}} = Api;
+			const audio = new Audio();
+			
+			/* Meme Sounds Class */
+			return class MemeSounds extends Plugin {
+				constructor() {
+					super();
+				}
+
+				getSettingsPanel() {
+					return this.buildSettingsPanel().getElement();
+				}
+	
+				onStart() {
+					Dispatcher.subscribe("MESSAGE_CREATE", this.messageEvent);
+				}
+				
+				messageEvent = async ({ channelId, message, optimistic }) => {
+					if (this.settings.setting.LimitChan && channelId != SelectedChannelStore.getChannelId())
+						return;
+
+					if (!optimistic) {
+						const count = (message.content.match(/nice/gmi) || []).length;
+				
+						for (let i = 0; i < count; i++) {
+							this.playNice();
+
+							await new Promise(r => setTimeout(r, this.settings.setting.delay));
+						}
+					}
+					
+					if (!optimistic) {
+						const count = (message.content.match(/noice/gmi) || []).length;
+				
+						for (let i = 0; i < count; i++) {
+							this.playNice();
+
+							await new Promise(r => setTimeout(r, this.settings.setting.delay));
+						}
+					}
+				};
+				
+				/* Players */
+				playNice() {
+					audio.src = "https://github.com/Lonk12/BetterDiscordPlugins/raw/main/MemeSounds/Sounds/noice.mp3";
+					audio.play();
+				}
+				
+				onStop() {
+					Dispatcher.unsubscribe("MESSAGE_CREATE", this.messageEvent);
+				}
+			}
+		} catch (e) { console.error(e); }};
+		return plugin(Plugin, Api);
+	})(global.ZeresPluginLibrary.buildPlugin(config));
+})();
