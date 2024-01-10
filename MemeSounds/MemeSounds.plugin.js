314530de7555ce2844adad0a381b1ef7dc1baec5
Lonk <51008428+Lonk12@users.noreply.github.com>
2024-01-09 20:26:04 -0500

    Update MemeSounds.plugin.js
    updated to main for 1.0 BABY

--- MemeSounds/MemeSounds.plugin.js
+++ MemeSounds/MemeSounds.plugin.js
@@ -182,7 +182,7 @@ module.exports = (() => {
 
 								for (let sound of [...queue.entries()].sort((a, b) => a[0] - b[0])) {
 									if (this.settings.toggle[sound[1].file.replace(/\..+$/, "")]) {
-										let audio = new Audio("https://github.com/Lonk12/BetterDiscordPlugins/raw/Broken-WIPs/MemeSounds/Sounds/" + sound[1].file);
+										let audio = new Audio("https://github.com/Lonk12/BetterDiscordPlugins/raw/main/MemeSounds/Sounds/" + sound[1].file);
 										audio.volume = this.settings.setting.volume;
 										audio.play();
 										await new Promise(r => setTimeout(r, sound[1].duration + this.settings.setting.delay));
